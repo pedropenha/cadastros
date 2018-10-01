6b3413f9a947a3e85c48e77bbb15b84adb5a1144
pedropenha <pedro.penha.martins@gmail.com>
2018-10-01 11:20:42 -0400

    Adicionando projeto

---
+++ add-user.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require 'config.php';
+require 'config2.php';
+
+if(isset($_POST['nome']) && !empty($_POST['nome'])){
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $senha = addslashes(MD5($_POST['senha']));
+
+    $sql = "SELECT * FROM users WHERE name = ?";
+    $sql = $pdo->prepare($sql);
+    $sql->bindValue(1, $nome);
+    $sql->execute();
+
+    if($sql->rowCount() == 0){
+        $query = "INSERT INTO users (name, email, password, signuptime) VALUES (?,?,?, NOW())";
+        $query = $pdo->prepare($query);
+        $query->bindValue(1, $nome);
+        $query->bindValue(2, $email);
+        $query->bindValue(3, $senha);
+        $query->execute();
+
+        header("Location: index.php");
+    }else{
+        ?>
+        <script>
+            alert('Usuario ja cadastrado');
+            window.location.href="index.php";
+        </script>
+        <?php
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Conta Bancaria</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css" rel="stylesheet"/>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/material.min.js"></script>
+    <script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+    <script src="assets/js/material-kit.js" type="text/javascript"></script>
+</head>
+<body style="background-color: transparent;">
+<div class="container">
+    <br/>
+    <form method="POST">
+        <h2>Adicionar usuarios <a href="./" class="btn btn-info btn-fab btn-fab-mini btn-round"
+                                  style="float: right;"><i class="material-icons">arrow_back</i></a></h2>
+        <br/>
+        <br/>
+        <h4>Nome: </h4>
+        <input type="text" class="form-control" name="nome" placeholder="Ex: Pedro Penha">
+        <h4>E-mail</h4>
+        <input type="email" class="form-control" name="email" placeholder="Ex: pedro@pedro.com">
+        <h4>Senha</h4>
+        <input type="password" class="form-control" name="senha" placeholder="******">
+
+        <input class="btn btn-primary" type="submit" value="Adicionar" />
+    </form>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+require 'config.php';
+
+if(isset($_POST['email']) && !empty($_POST['email'])) {
+    $email = addslashes($_POST['email']);
+    $senha = addslashes($_POST['senha']);
+
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = ? AND senha = ?");
+    $sql->bindValue(1, $email);
+    $sql->bindValue(2, md5($senha));
+    $sql->execute();
+
+    if($sql->rowCount() > 0) {
+        $sql = $sql->fetch();
+
+        $_SESSION['banco'] = $sql['id'];
+        header("Location: index.php");
+        exit;
+    }
+
+
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Suporte</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css" rel="stylesheet"/>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/material.min.js"></script>
+    <script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+    <script src="assets/js/material-kit.js" type="text/javascript"></script>
+
+</head>
+<body class="signup-page">
+<div class="wrapper">
+    <div class="header header-filter" style="background-image: url('assets/img/city.jpg'); background-size: cover; background-position: top center;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                    <div class="card card-signup">
+                        <form class="form" method="POST">
+                            <div class="header header-info text-center">
+                                <h4>Logar</h4>
+                            </div>
+                            <div class="content">
+
+                                <div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">face</i>
+										</span>
+                                    <input type="text" class="form-control" placeholder="E-mail" name="email"/>
+                                </div>
+
+                                <div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">lock_outline</i>
+										</span>
+                                    <input type="password" placeholder="Senha" class="form-control" name="senha"/>
+                                </div>
+                            </div>
+                            <div class="footer text-center">
+                                <button type="submit" class="btn btn-success btn-round">
+                                     Entrar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
