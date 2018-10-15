1b8a8b404c0c19bd3f277af31cb284176b01d540
Pedro Penha <pedro.penha.martins@gmail.com>
2018-10-15 14:53:14 -0400

    Liberando espaço

--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if(isset($_POST['email']) && !empty($_POST['email'])) {
 </head>
 <body class="signup-page">
 <div class="wrapper">
-    <div class="header header-filter" style="background-image: url('assets/img/city.jpg'); background-size: cover; background-position: top center;">
+    <div class="header header-filter" style="background-image: url('assets/img/bg2.jpeg'); background-size: cover; background-position: top center;">
         <div class="container">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
